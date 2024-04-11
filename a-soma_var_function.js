83ec54c61bd714fc8f4b3a76b588f29a662a92b3
ayran13062007 <159441847+ayran13062007@users.noreply.github.com>
2024-04-11 09:07:09 -0300

    Add files via upload

---
+++ a-soma_var_function.js
@@ -0,0 +1,12 @@
+const num1 = 25;
+const num2 = 25;
+const resultado = num1 + num2;
+
+///MESMA FUNÇÂO DE CODIGO - FAZnodeEM A MESMA COISA
+
+function soma(num1, num2)
+{
+    return num1 + num2;
+}
+    console.log(soma(5, 5))
+    console.log(resultado)
---
+++ b-escolhanumero.js
@@ -0,0 +1,11 @@
+const num = 50;
+
+if (num >= 0 && num <= 10){
+    console.log('numero entre zero e dez');
+} else if (num > 10 && num <= 20) {
+console.log('numero entre dez e vinte');
+}else if (num > 20 && num <= 30) {
+console.log('numero entre vinte e trinta');
+} else {
+    console.log('numero MAIOR que 30');
+}
